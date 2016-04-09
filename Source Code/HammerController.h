b171f410fb3f85430057003e67f1bfce4d9b3274
Steve White <stepwhite@gmail.com>
2016-04-08 21:43:54 -0700

    Add HammerController, an object that can (barely) speak the Hammer protocol.  Enough to read memory off the device.
    With this it'll handshake to Debugger/Newtsbug Connection on
    the device (aka InitSerialDebugging).
    
    It'll try to probe to figure out what device it's running on
    (via reads to gROMManufacturer, gHardwareType, gROMVersion)
    
    It'll try to probe to figure out the ROM size, by checking 1MB
    increments to see if they repeat the word found at 0x00000000
    (e.g. due to ROM mirroring, this should happen).
    
    Then it'll attempt to download the ROM at a speed that is best
    described as "glacial".  Currently it's reading 12 bytes per
    request.  Given an 8MB ROM...
    
    And afterwards, you'll have a hung Newton that'll need to be
    power cycled. Sniffing traffic between a Newton and Hammer
    shows that the "Go" command sends 0x10 0x10 0x01, but this
    doesn't seem to work here...

---
+++ Source Code/HammerController.h
@@ -0,0 +1,25 @@
+//
+//  HammerController.h
+//  NewTen
+//
+//  Created by Steve White on 4/8/16.
+//
+//
+
+#import <Foundation/Foundation.h>
+#import "NewtonConnection.h"
+
+@interface HammerController : NSObject {
+  NSString *_devicePath;
+  NewtonConnection *_connection;
+  volatile BOOL _giveUp;
+
+  uint8_t _sendBuf[MAX_HEAD_LEN + MAX_INFO_LEN];
+  uint8_t _recvBuf[MAX_HEAD_LEN + MAX_INFO_LEN];
+  int _recvBufLen;
+}
+
+- (void) setDevicePath:(NSString *)devicePath;
+- (NSString *) devicePath;
+
+@end
---
+++ Source Code/HammerController.m
@@ -0,0 +1,352 @@
+//
+//  HammerController.m
+//  NewTen
+//
+//  Created by Steve White on 4/8/16.
+//
+//
+
+#import "HammerController.h"
+
+#import "AppDelegate.h"
+#import "NewtonConnection.h"
+
+#define DEBUG 0
+
+// From http://newton.vyx.net/revenge/
+enum {
+  CommandOpen = 0x00,
+  CommandClose = 0x01,
+  CommandReadMemory = 0x02,
+  CommandWriteMemory = 0x03,
+  CommandReadRegisters = 0x08,
+  CommandWriteRegisters = 0x09,
+  CommandExecute = 0x10,
+  CommandReadPhysicalMemory = 0x18,
+  CommandWritePhysicalMemory = 0x19,
+  CommandUnknown = 0x1a,
+  CommandStop = 0x1b,
+  CommandGo = 0x1c,
+  CommandAck = 0x40,
+  CommandPing = 0x78,
+};
+
+enum {
+  ResponseStopWithStatus = 0x20,
+  ResponseFatal = 0x5e,
+  ResponseResult = 0x5f,
+  ResponseError = 0x60,
+  ResponseInquiry = 0x80,
+  ResponsePong = 0x81,
+  ResponseReset = 0x7f,
+};
+
+@implementation HammerController
+
+- (void) dealloc {
+  if (_devicePath) {
+    [_devicePath release], _devicePath = nil;
+  }
+  if (_connection) {
+    [_connection release], _connection = nil;
+  }
+
+  [super dealloc];
+}
+
+- (void) cancel {
+  [_connection cancel];
+  _giveUp = YES;
+}
+
+- (void) setDevicePath:(NSString *)devicePath {
+  if (_devicePath) {
+    [_devicePath release], _devicePath = nil;
+  }
+  _devicePath = [devicePath retain];
+}
+
+- (NSString *) devicePath {
+  return [[_devicePath retain] autorelease];
+}
+
+- (AppDelegate *) delegate {
+  return (id)[NSApp delegate];
+}
+
+#pragma mark - Reading
+- (BOOL) readFrame {
+	int status = 0;
+	
+	memset(_recvBuf, 0x00, sizeof(_recvBuf));
+	_recvBufLen = 0;
+	
+	while ( (status = [_connection receiveFrame:_recvBuf length:&_recvBufLen]) < 0 )
+	{
+		if ( _giveUp ) {
+			[[NSException exceptionWithName:@"HammerControllerCancelled" reason:@"_giveUp == true" userInfo:nil] raise];
+			return NO;
+		}
+	}
+	
+	return YES;
+}
+
+- (BOOL) readDataForResponse:(uint8_t)response {
+	do {
+		BOOL success = [self readFrame];
+		if (success == NO) {
+			return NO;
+		}
+		
+		if (_recvBuf[0] == response) {
+			return YES;
+		}
+	}
+	while (_giveUp == false);
+	return NO;
+}
+
+#pragma mark - Sending
+- (void) sendFrameWithCommand:(uint8_t)command body:(uint8_t *)body length:(uint8_t)length {
+  memset(_sendBuf, 0x00, sizeof(_sendBuf));
+  _sendBuf[0] = command;
+  
+  if (length > 0) {
+    if (length - 1 > sizeof(_sendBuf)) {
+      NSLog(@"Length would exceed _sendBuf size. truncating!");
+      length = (uint8_t)sizeof(_sendBuf) - 1;
+    }
+    memcpy(&_sendBuf[1], body, length);
+  }
+
+  [_connection sendFrame:&_sendBuf[0] header:NULL length:length + 1];
+  if (_giveUp == true) {
+    [[NSException exceptionWithName:@"HammerControllerCancelled" reason:@"_giveUp == true" userInfo:nil] raise];
+  }
+}
+
+- (void) sendFrameWithCommand:(uint8_t)command {
+  [self sendFrameWithCommand:command body:NULL length:0];
+}
+
+- (void) sendCloseCommand {
+  [self sendFrameWithCommand:CommandClose];
+}
+
+- (void) sendGoCommand {
+  [self sendFrameWithCommand:CommandAck];
+  
+  uint8_t goCommand[] = { 0x00, 0x00, 0x00, 0x00 };
+  [self sendFrameWithCommand:CommandGo body:&goCommand[0] length:sizeof(goCommand)];
+  [self readDataForResponse:ResponseResult];
+/*
+  uint8_t execCommand[] = { 0x10, 0x01 };
+  [self sendFrameWithCommand:CommandExecute body:&execCommand[0] length:sizeof(execCommand)];
+  [self readDataForResponse:ResponseResult];
+ */
+}
+
+- (void) sendStopCommand {
+  uint8_t stopCmd[] = { 'S', 'T', 'O', 'P'};
+  [self sendFrameWithCommand:CommandStop body:&stopCmd[0] length:sizeof(stopCmd)];
+}
+
+- (void) sendReadMemoryCommandWithAddress:(uint32_t)address length:(uint32_t)length {
+  uint8_t readMemCmd[] = {
+    (address      ) & 0xff,
+    (address >>  8) & 0xff,
+    (address >> 16) & 0xff,
+    (address >> 24) & 0xff,
+    (length      ) & 0xff,
+    (length >>  8) & 0xff,
+    (length >> 16) & 0xff,
+    (length >> 24) & 0xff,
+  };
+  
+  [self sendFrameWithCommand:CommandReadPhysicalMemory body:&readMemCmd[0] length:sizeof(readMemCmd)];
+  [self readDataForResponse:ResponseResult];
+}
+
+#pragma mark -
+- (NSString *) deviceDescription {
+  uint32_t *recvData = (uint32_t *)&_recvBuf[1];
+
+  NSMutableString *deviceDescription = [[NSMutableString alloc] init];
+
+  // Read gROMManufacturer
+  [self sendReadMemoryCommandWithAddress:0x000013f0 length:4];
+  uint32_t romManufacturer = HTONL(recvData[0]);
+  switch (romManufacturer) {
+    case 0x01000000:
+      [deviceDescription appendString:@"Apple"];
+      break;
+    case 0x10000100:
+      [deviceDescription appendString:@"Sharp"];
+      break;
+    default:
+      [deviceDescription appendFormat:@"Unknown (%08x)", romManufacturer];
+      break;
+  }
+  [deviceDescription appendString:@" "];
+
+  // Read gHardwareType
+  [self sendReadMemoryCommandWithAddress:0x000013ec length:4];
+  uint32_t hardwareType = HTONL(recvData[0]);
+  switch (hardwareType) {
+    case 0x10001000:
+      [deviceDescription appendString:@"MessagePad"];
+      break;
+    case 0x10002000:
+      [deviceDescription appendString:@"Bic"];
+      break;
+    case 0x10003000:
+      [deviceDescription appendString:@"MessagePad 2000/2100"];
+      break;
+    case 0x10004000:
+      [deviceDescription appendString:@"eMate 300"];
+      break;
+    case 0x00726377:
+      [deviceDescription appendString:@"Lindy"];
+      break;
+    default:
+      [deviceDescription appendFormat:@"Unknown (%08x)", hardwareType];
+      break;
+  }
+  
+  [deviceDescription appendString:@" "];
+
+  // Read gROMVersion
+  [self sendReadMemoryCommandWithAddress:0x000013dc length:4];
+  uint32_t romVersion = HTONL(recvData[0]);
+  [deviceDescription appendFormat:@"v%i.%i", romVersion >> 16, romVersion & 0xffff];
+
+  return [deviceDescription autorelease];
+}
+
+- (void) handleHandshake {
+  // Wait to receive an inquiry
+  [self readDataForResponse:ResponseInquiry];
+  
+  // Send handshake
+  [[self delegate] updateStatus:NSLocalizedString(@"Handshaking...", @"Handshaking...")];
+  uint8_t handshake[] = {0x00, 0x05, 0x00, 0x00, 0x00, 0x00, 0x82, 0x00, 0x00};
+  [self sendFrameWithCommand:CommandUnknown body:&handshake[0] length:sizeof(handshake)];
+  
+  // We expect two result frames
+  [self readDataForResponse:ResponseResult];
+  [self readDataForResponse:ResponseResult];
+  
+  // Followed by a stop with status frame
+  [self readDataForResponse:ResponseStopWithStatus];
+  
+  // To which we send a STOP
+  [self sendStopCommand];
+  
+  // Followed by another stop with status frame
+  [self readDataForResponse:ResponseStopWithStatus];
+}
+
+- (uint32_t) determineROMSize {
+  uint32_t *recvData = (uint32_t *)&_recvBuf[1];
+  [self sendReadMemoryCommandWithAddress:0 length:4];
+  uint32_t firstWord = recvData[0];
+  
+  uint32_t addr = 1024 * 1024;
+  while (true) {
+    [self sendReadMemoryCommandWithAddress:addr length:4];
+    uint32_t thisWord = recvData[0];
+    if (thisWord == firstWord) {
+      break;
+    }
+    
+    addr += 1024 * 1024;
+    if (addr > 16 * 1024 * 1024) {
+      NSLog(@"Exceeded 16MB. Giving up");
+      addr = 0;
+      break;
+    }
+  }
+  return addr;
+}
+
+- (NSData *) dumpROMOfLength:(uint32_t)length {
+  NSMutableData *romData = [[[NSMutableData alloc] initWithCapacity:length] autorelease];
+
+  uint32_t *recvData = (uint32_t *)&_recvBuf[1];
+  uint32_t addr = 0;
+  
+  AppDelegate *delegate = [self delegate];
+  [delegate updateProgressMax:[NSNumber numberWithInt:length]];
+
+#define READ_SIZE 12
+  while (addr < length) {
+    [self sendReadMemoryCommandWithAddress:addr length:READ_SIZE];
+    
+    [romData appendBytes:recvData length:READ_SIZE];
+    addr += READ_SIZE;
+    [delegate updateProgress:[NSNumber numberWithInt:addr]];
+  }
+  
+  return romData;
+}
+
+#pragma mark -
+- (void) main {
+
+  AppDelegate *delegate = [self delegate];
+  [delegate showStatusSheet];
+  [delegate updateStatus:NSLocalizedString(@"Setting up serial port...", @"Setting up serial port...")];
+  
+  _connection = [[NewtonConnection connectionWithDevicePath:[self devicePath] speed:57600] retain];
+  
+  // Wait for Newton to connect
+  
+  [delegate updateStatus:NSLocalizedString(@"Waiting for Newton Debugger connection...", @"Waiting for Newton Debugger connection...")];
+  
+  if (_giveUp == true) {
+    goto bail;
+  }
+
+  @try {
+    [self handleHandshake];
+
+    [delegate updateStatus:NSLocalizedString(@"Detecting Newton type...", @"Detecting Newton type...")];
+    NSString *deviceDescription = [self deviceDescription];
+    [delegate updateStatus:deviceDescription];
+    
+    uint32_t romSize = [self determineROMSize];
+    if (romSize > 0) {
+      NSString *humanSize = [NSString stringWithFormat:@" (%i MB)", romSize / 1024 / 1024];
+      deviceDescription = [deviceDescription stringByAppendingString:humanSize];
+      [delegate updateStatus:deviceDescription];
+
+      NSData *romData = [self dumpROMOfLength:romSize];
+      if (romData != nil) {
+        [delegate saveData:romData withFilename:[deviceDescription stringByAppendingPathExtension:@"rom"]];
+      }
+    }
+    [delegate updateStatus:NSLocalizedString(@"Finished", @"Finished")];
+  }
+  @catch (id e) {
+    NSLog(@"%s caught: %@", __PRETTY_FUNCTION__, e);
+  }
+  @finally {
+    // Try to re-enable the Newton
+    [delegate updateStatus:NSLocalizedString(@"Disconnecting", @"Disconnecting")];
+    @try {
+      [self sendGoCommand];
+    }
+    @catch (id e) {}
+  }
+
+bail:
+  
+  [_connection cancel];
+  [_connection release];
+  _connection = nil;
+  
+  [delegate hideStatusSheet];
+}
+
+@end
