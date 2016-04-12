33a05857b81359bfb4bfa61478216115ffab666d
Steve White <stepwhite@gmail.com>
2016-04-11 17:47:45 -0700

    A class that can stitch together many images, selectively hide/display them, and optionally point at rects in the image.
    (Used to display screenshots of a Newton, pointing at what one
    should tap on to make the ROM downloading happen)

---
+++ Source Code/ScreencastView.h
@@ -0,0 +1,28 @@
+//
+//  ScreencastView.h
+//  Newt Dumper
+//
+//  Created by Steve White on 4/10/16.
+//  Copyright © 2016 Steve White. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface HighlightView : NSView
+@property (assign, nonatomic) NSRect highlightRect;
+@end
+
+@interface ScreencastView : NSView {
+  NSMutableDictionary *_imageViewsByIdentifier;
+  HighlightView *_highlightView;
+}
+
+- (CGSize) contentSize;
+- (void) removeAllImages;
+
+- (NSArray *) allIdentifiers;
+- (void) addImageNamed:(NSString *)imageName atPoint:(NSPoint)point withIdentifier:(NSString *)identifier;
+- (void) setHidden:(BOOL)hidden forImageWithIdentifier:(NSString *)identifier;
+- (void) highlightRect:(NSRect)highlightRect;
+
+@end
---
+++ Source Code/ScreencastView.m
@@ -0,0 +1,141 @@
+//
+//  ScreencastView.m
+//  Newt Dumper
+//
+//  Created by Steve White on 4/10/16.
+//  Copyright © 2016 Steve White. All rights reserved.
+//
+
+#import "ScreencastView.h"
+
+
+@implementation ScreencastView 
+
+- (void) awakeFromNib {
+  [super awakeFromNib];
+
+  _highlightView = [[HighlightView alloc] initWithFrame:self.bounds];
+  _highlightView.autoresizingMask = NSViewWidthSizable | NSViewHeightSizable;
+  [self addSubview:_highlightView];
+}
+
+- (void) dealloc {
+  [_highlightView removeFromSuperview];
+  [_highlightView release], _highlightView = nil;
+  [_imageViewsByIdentifier release], _imageViewsByIdentifier = nil;
+  
+  [super dealloc];
+}
+
+- (void) removeAllImages {
+  for (NSString *anIdentifier in _imageViewsByIdentifier) {
+    NSView *view = [_imageViewsByIdentifier objectForKey:anIdentifier];
+    [view removeFromSuperview];
+  }
+  [_imageViewsByIdentifier removeAllObjects];
+  [_highlightView setHighlightRect:NSZeroRect];
+}
+
+- (CGSize) contentSize {
+  CGFloat minX=CGFLOAT_MAX,minY=CGFLOAT_MAX,maxX=0,maxY=0;
+  for (NSString *anImageIdentifier in _imageViewsByIdentifier) {
+    NSView *imageView = _imageViewsByIdentifier[anImageIdentifier];
+    NSRect frame = imageView.frame;
+    minX = MIN(minX, NSMinX(frame));
+    minY = MIN(minY, NSMinY(frame));
+    maxX = MAX(maxX, NSMaxX(frame));
+    maxY = MAX(maxY, NSMaxY(frame));
+  }
+  return CGSizeMake(maxX, maxY);
+}
+
+- (BOOL) isFlipped {
+  return YES;
+}
+
+- (NSArray *) allIdentifiers {
+  return [_imageViewsByIdentifier allKeys];
+}
+
+- (void) setHidden:(BOOL)hidden forImageWithIdentifier:(NSString *)identifier {
+  NSImageView *imageView = [_imageViewsByIdentifier objectForKey:identifier];
+  if (imageView == nil) {
+    NSLog(@"%s couldn't find image view for identifier:%@", __PRETTY_FUNCTION__, identifier);
+  }
+  else {
+    imageView.hidden = hidden;
+  }
+}
+
+- (void) addImageNamed:(NSString *)imageName atPoint:(NSPoint)point withIdentifier:(NSString *)identifier {
+  NSImage *image = [NSImage imageNamed:imageName];
+  if (image == nil) {
+    NSLog(@"%s imageNamed:%@ returned nil!", __PRETTY_FUNCTION__, imageName);
+    return;
+  }
+  
+  NSRect viewFrame = NSZeroRect;
+  viewFrame.origin = point;
+  viewFrame.size = image.size;
+
+  NSImageView *imageView = [[[NSImageView alloc] initWithFrame:viewFrame] autorelease];
+  imageView.image = image;
+
+  [self addSubview:imageView positioned:NSWindowBelow relativeTo:_highlightView];
+  
+  if (_imageViewsByIdentifier == nil) {
+    _imageViewsByIdentifier = [[NSMutableDictionary alloc] init];
+  }
+  
+  if (identifier != nil) {
+    [_imageViewsByIdentifier setObject:imageView forKey:identifier];
+  }
+}
+
+- (void) highlightRect:(NSRect)highlightRect {
+  _highlightView.highlightRect = highlightRect;
+}
+
+@end
+
+
+@implementation HighlightView
+
+- (BOOL) isFlipped {
+  return YES;
+}
+
+- (void) setHighlightRect:(NSRect)highlightRect {
+  _highlightRect = highlightRect;
+  [self setNeedsDisplay:YES];
+}
+
+- (void) drawRect:(NSRect)dirtyRect {
+  [super drawRect:dirtyRect];
+  
+  if (NSEqualRects(NSZeroRect, _highlightRect) == YES) {
+    return;
+  }
+  
+  NSBezierPath *arrowPath = [NSBezierPath bezierPath];
+  
+  NSPoint topCenter = NSMakePoint(NSMidX(_highlightRect), NSMinY(_highlightRect) - 5);
+  
+  [arrowPath moveToPoint:topCenter];
+  [arrowPath lineToPoint:NSMakePoint(topCenter.x - 5, topCenter.y - 5)];
+  [arrowPath lineToPoint:topCenter];
+  [arrowPath lineToPoint:NSMakePoint(topCenter.x + 5, topCenter.y - 5)];
+  [arrowPath lineToPoint:topCenter];
+  [arrowPath lineToPoint:NSMakePoint(topCenter.x, topCenter.y - 50)];
+  [arrowPath setLineWidth:2];
+  
+  [[NSColor redColor] setStroke];
+  [arrowPath stroke];
+  
+  NSRect outline = NSInsetRect(_highlightRect, -1, -1);
+  NSBezierPath *outlinePath = [NSBezierPath bezierPathWithRect:outline];
+  [outlinePath setLineWidth:2];
+  [outlinePath stroke];
+}
+
+@end
